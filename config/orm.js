8752dfc24472d0c5fd6dfd01ac9ed948f15445b8
Nicole <nalbrigh@kent.edu>
2019-05-02 21:22:44 -0400

    deleted more console logs

--- config/orm.js
+++ config/orm.js
@@ -75,7 +75,7 @@ var orm = {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
-    console.log(objColVals)
+    // console.log(objColVals)
     queryString += objToSql(objColVals);
     queryString += " WHERE ";
     queryString += condition;
--- controllers/drinks_controller.js
+++ controllers/drinks_controller.js
@@ -11,7 +11,7 @@ router.get("/", function(req, res) {
     var hbsObject = {
       drinks: data
     };
-    console.log(hbsObject);
+    // console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
