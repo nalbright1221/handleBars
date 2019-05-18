f3e7d1a9452ac08380532b8e0a1fd7178d6fc5f0
Nicole <nalbrigh@kent.edu>
2019-05-18 10:58:38 -0400

    attempting to push to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,29 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "yeloc1221",
-  database: "drinks_db"
-});
+var connection;
+
+// = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "yeloc1221",
+//   database: "drinks_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+  connetion = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user:  'root',
+    password: 'yeloc1221',
+    database: 'drinks_db'
+  });
+};
+
+// connection.connect();
 
 // Make connection.
 connection.connect(function(err) {
